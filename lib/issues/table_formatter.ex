a5eba924232c82a0fee2afccec70e06e94925d35
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-08-01 20:10:52 -0400

    Add and remove docs where necessary

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,9 +1,21 @@
 defmodule Issues.TableFormatter do
+
   @fields_to_print [
     { "number", :num , "#"},
     { "created_at", :str },
     { "title", :str }
   ]
+
+  @doc """
+  Requires a list of maps. It prints the list as a table, with the columns
+  normalized based on the longest field entry.
+  """
+  def print(list) do
+    remove_clutter(list)
+    |> get_column_widths
+    |> print_justified_table(list)
+  end
+
 
   @doc """
   Helper which returns the first fields of the fields to print list in a list.
@@ -14,8 +26,9 @@ defmodule Issues.TableFormatter do
   def get_field_as_string({ field, _, _ }), do: field
   def get_field_as_string({ field, _ }), do: field
 
+
   @doc """
-  Helpe which returns the display strings of the fields to print list.
+  Helper which returns the display strings of the fields to print list.
   """
   def get_field_display_names() do
     Enum.map(@fields_to_print, &(get_display_string(&1)))
@@ -23,11 +36,6 @@ defmodule Issues.TableFormatter do
   def get_display_string({ _, _, disp }), do: disp
   def get_display_string({ field, _ }), do: field
 
-  def print(list) do
-    remove_clutter(list)
-    |> get_column_widths
-    |> print_justified_table(list)
-  end
 
   @doc """
   The list we get has a ton of useless data. We get rid of that for ease of
@@ -37,18 +45,39 @@ defmodule Issues.TableFormatter do
     Enum.map(list, &(Map.take(&1, get_fields_as_strings)))
   end
 
+
+  @doc """
+  Get the max width required for each column.
+  """
   def get_column_widths(list) do
     @fields_to_print
     |> Enum.map(&(max_field_length(&1, list)))  # Get required column widths
   end
 
-  defp print_justified_table(widths, list) do
+
+  @doc """
+  Prints each part of the table, while maintaining the widths for each column.
+  """
+  def print_justified_table(widths, list) do
     print_header(widths)
     print_delimiting_line(widths)
     Enum.each(list, &(print_line(&1, widths)))
   end
 
-  defp form_header(widths) do
+
+  @doc """
+  Prints the @fields_to_prints display names using the widths.
+  """
+  def print_header(widths) do
+    form_header(widths)
+    |> Enum.each(&(IO.write(&1)))
+  end
+
+
+  @doc """
+  Create the header of the table.
+  """
+  def form_header(widths) do
     get_field_display_names
     |> Enum.zip(widths)
     |> Enum.map(fn({ str, size }) -> String.ljust(str, size) end)
@@ -58,14 +87,15 @@ defmodule Issues.TableFormatter do
   defp add_delimeters([]), do: ["\n" | []]
   defp add_delimeters([], _), do: ["\n" | []]
   defp add_delimeters([ head | [] ], _), do: [ head | add_delimeters([])]
-  defp add_delimeters([ head | tail ], str), do: [ head <> str | add_delimeters(tail, str)]
-
-  defp print_header(widths) do
-    form_header(widths)
-    |> Enum.each(&(IO.write(&1)))
+  defp add_delimeters([ head | tail ], str) do
+    [ head <> str | add_delimeters(tail, str)]
   end
 
-  defp print_delimiting_line(widths) do
+
+  @doc """
+  We need a horizontal line to distinguish the header from the body.
+  """
+  def print_delimiting_line(widths) do
     widths
     |> Enum.map(&(String.pad_trailing("", &1, "-")))
     |> add_delimeters("-+-")
@@ -79,6 +109,12 @@ defmodule Issues.TableFormatter do
     |> Enum.each(&(IO.write(&1)))
   end
 
+  defp get_line_data(line) do
+    get_fields_as_strings()
+    |> Enum.map(&(line[&1]))
+    |> Enum.map(&(parse_if_int(&1)))
+  end
+
   defp format_line_data(line, widths) do
     line
     |> Enum.zip(widths)
@@ -86,23 +122,9 @@ defmodule Issues.TableFormatter do
     |> add_delimeters(" | ")
   end
 
-  defp get_line_data(line) do
-    get_fields_as_strings()
-    |> Enum.map(&(line[&1]))
-    |> Enum.map(&(parse_if_int(&1)))
-  end
-
   defp parse_if_int(n) when is_integer(n), do: Integer.to_string(n, 10)
   defp parse_if_int(n), do: n
 
-  @doc """
-  This takes the list of Maps which are the git issues, and returns the maximum
-  string length required by them. So if the longest title has 31 charachters,
-  this returns 31.
-
-  :num pattern exists since we need to parse numeric fields into strings before
-  getting the length, whereas :str can skip this step.
-  """
   defp max_field_length({ field, type, _ }, list) do
     max_field_length({ field, type }, list)
   end
